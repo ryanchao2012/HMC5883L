f327e95deeb6aa80aea5a27eead91477af92736e
Ryan Chao <ryanchao2012@gmail.com>
2015-03-24 16:49:53 +0800

    build basic functions

--- HMC5883L.cpp
+++ HMC5883L.cpp
@@ -23,9 +23,531 @@
 //
 /*******************************************************************************/
 
-#include <Arduino.h>
+#if ARDUINO >= 100
+ #include "Arduino.h"
+#else
+ #include "WProgram.h"
+#endif
+
+#include <Wire.h>
+
 #include "HMC5883L.h"
 
+uint16_t calibRef[8] = {1370, 1090, 820, 660, 440, 390, 330, 230};
+/**************************************************************************/
+/*!
+    @brief  Instantiates a new HMC5883L class with default setting
+*/
+/**************************************************************************/
+HMC5883L::HMC5883L()
+{
+    // default setting
+    _operateMode    = HMC5883L_OPMODE_CONTIUOUS;
+    _deviceRange    = HMC5883L_RANGE_5_6_GA;
+    _measureMode    = HMC5883L_MEASMODE_NORMAL;
+    _dataRate       = HMC5883L_DATARATE_75_HZ;
+    _sampleAvg      = HMC5883L_1_AVG_SAMPLE;
+
+}
+
+
+/**************************************************************************/
+/*!
+    @brief  Setups the HW (reads coefficients values, etc.)
+*/
+/**************************************************************************/
+bool HMC5883L::begin()
+{
+    uint8_t status;
+
+    Wire.begin();
+    if(!checkID()) return false;
+
+    setRegCfgA(_measureMode, _dataRate, _sampleAvg);
+    setRegCfgB(_deviceRange);
+    setRegMode(_operateMode);
+
+    status = getStatus();
+    printStatus(status);
+    resetData();
+    // Serial.println(calibRef[(uint8_t)_deviceRange]);
+    return true;
+}
+
+void HMC5883L::setRegCfgA(measureMode_t measMode, dataRate_t rate, sampleAvg_t sample)
+{
+    uint8_t newData =   (measMode)
+                      | (rate << 2)
+                      | (sample << 5);
+
+    newData &= REG_A_DEFAULT;
+    writeRegister(HMC5883L_REG_CFG_A, newData);
+}
+
+
+void HMC5883L::setRegCfgB(deviceRange_t range)
+{
+    uint8_t newData = (range << 5);
+    newData &= REG_B_DEFAULT;
+    writeRegister(HMC5883L_REG_CFG_B, newData);
+}
+
+
+void HMC5883L::setRegMode(operateMode_t opMode)
+{
+    uint8_t newData = opMode;
+    newData &= REG_MODE_DEFAULT;
+    writeRegister(HMC5883L_REG_MODE, newData);
+}
+
+
+void HMC5883L::setRange(deviceRange_t range)
+{
+    if( _deviceRange == range) return;
+    uint8_t tempData, newData;
+    tempData = readRegister(HMC5883L_REG_CFG_B);
+    /* Update the data */
+    newData = tempData & 0b00011111;
+    newData |= (range << 5);
+    newData &= REG_B_DEFAULT;
+    /* Write the register back to the IC */
+    writeRegister(HMC5883L_REG_CFG_B, newData);
+    /* Keep track of the current range (to avoid readbacks) */
+    _deviceRange = range;
+}
+
+
+void HMC5883L::setMeasureMode(measureMode_t measMode)
+{
+    if(_measureMode == measMode) return;
+    uint8_t tempData, newData;
+    tempData = readRegister(HMC5883L_REG_CFG_A);
+    /* Update the data */
+    newData = tempData & 0b11111100;
+    newData |= measMode;
+    newData &= REG_A_DEFAULT;
+    /* Write the register back to the IC */
+    writeRegister(HMC5883L_REG_CFG_A, newData);
+    /* Keep track of the current measurement mode (to avoid readbacks) */
+    _measureMode = measMode;
+}
+
+
+void HMC5883L::setSampleAvg(sampleAvg_t sample)
+{
+    if(_sampleAvg == sample) return;
+    uint8_t tempData, newData;
+    tempData = readRegister(HMC5883L_REG_CFG_A);
+    /* Update the data */
+    newData = tempData & 0b10011111;
+    newData |= (sample << 5);
+    newData &= REG_A_DEFAULT;
+    /* Write the register back to the IC */
+    writeRegister(HMC5883L_REG_CFG_A, newData);
+    /* Keep track of the number of samples averaged (to avoid readbacks) */
+    _sampleAvg = sample;
+}
+
+
+void HMC5883L::setDataRate(dataRate_t rate)
+{
+    if(_dataRate == rate) return;
+    uint8_t tempData, newData;
+    tempData = readRegister(HMC5883L_REG_CFG_A);
+    /* Update the data */
+    newData = tempData & 0b11100011;
+    newData |= (rate << 2);
+    newData &= REG_A_DEFAULT;
+    /* Write the register back to the IC */
+    writeRegister(HMC5883L_REG_CFG_A, newData);
+    /* Keep track of the data rate (to avoid readbacks) */
+    _dataRate = rate;
+}
+
+
+void HMC5883L::setOperateMode(operateMode_t opMode)
+{
+    if(_operateMode == opMode) return;
+    uint8_t tempData, newData;
+    tempData = readRegister(HMC5883L_REG_MODE);
+    /* Update the data */
+    newData = tempData & 0b11111100;
+    newData |= opMode;
+    newData &= REG_MODE_DEFAULT;
+    /* Write the register back to the IC */
+    writeRegister(HMC5883L_REG_MODE, newData);
+    /* Keep track of the current operating mode (to avoid readbacks) */
+    _operateMode = opMode;
+}
+
+
+/**************************************************************************/
+/*!
+    @brief  Abstract away platform differences in Arduino wire library
+*/
+/**************************************************************************/
+inline void HMC5883L::i2cwrite(uint8_t x) 
+{
+  #if ARDUINO >= 100
+  Wire.write((uint8_t)x);
+  #else
+  Wire.send(x);
+  #endif
+}
+
+
+/**************************************************************************/
+/*!
+    @brief  Abstract away platform differences in Arduino wire library
+*/
+/**************************************************************************/
+inline uint8_t HMC5883L::i2cread(void) 
+{
+  #if ARDUINO >= 100
+  return Wire.read();
+  #else
+  return Wire.receive();
+  #endif
+}
+
+
+/**************************************************************************/
+/*!
+    @brief  Check LOCK or READY status in device
+    LOCK:
+        This bit is set when:
+        1. some but not all for of the six data output registers have been read,
+        2. Mode register has been read.
+        
+        When this bit is set, the six data output registers are locked 
+        and any new data will not be placed in these register until 
+        one of these conditions are met:
+        1. all six bytes have been read, 
+        2. the mode register is changed,
+        3. the measurement configuration (CRA) is changed,
+        4. power is reset.
+
+    READY:
+        Set when data is written to all six data registers. 
+        Cleared when device initiates a write to the data output registers 
+        and after one or more of the data output registers are written to. 
+        When RDY bit is clear it shall remain cleared for a 250 μs. 
+        DRDY pin can be used as an alternative to the status register 
+        for monitoring the device for measurement data.
+*/
+/**************************************************************************/
+uint8_t HMC5883L::getStatus(void)
+{
+    return readRegister(HMC5883L_REG_STATUS);  
+}
+
+
+/**************************************************************************/
+/*!
+    @brief  Writes 8-bits to the specified destination register
+*/
+/**************************************************************************/
+void HMC5883L::writeRegister(uint8_t reg, uint8_t value) 
+{
+    Wire.beginTransmission(HMC5883L_ADDRESS);
+    i2cwrite((uint8_t)reg);
+    i2cwrite((uint8_t)(value));
+    Wire.endTransmission();
+}
+
+
+/**************************************************************************/
+/*!
+    @brief  Reads 8-bits from the specified register
+*/
+/**************************************************************************/
+uint8_t HMC5883L::readRegister(uint8_t reg) 
+{
+    uint8_t data;
+    Wire.beginTransmission(HMC5883L_ADDRESS);
+    i2cwrite(reg);
+    Wire.endTransmission();
+    Wire.requestFrom(HMC5883L_ADDRESS, 1);
+    data = i2cread();
+    Wire.endTransmission();
+    return data; 
+}
+
+
+/**************************************************************************/
+/*!
+    @brief  Used to identify the device.
+*/
+/**************************************************************************/
+bool HMC5883L::checkID(void)
+{
+    uint16_t _idA, _idB, _idC;
+    _idA = readRegister(HMC5883L_REG_ID_A);
+    _idB = readRegister(HMC5883L_REG_ID_B);
+    _idC = readRegister(HMC5883L_REG_ID_C);
+    return ((_idA + _idB + _idC) == ID_SUM) ? true : false;
+}
+
+
+bool HMC5883L::getRawData(void)
+{
+    uint8_t   status, discard;
+    short     reqNum = 6;
+
+    status = getStatus();
+
+    Wire.beginTransmission(HMC5883L_ADDRESS);
+    i2cwrite(HMC5883L_REG_X_MSB);
+    Wire.endTransmission();
+    Wire.requestFrom(HMC5883L_ADDRESS, reqNum);
+    
+    if(!(status & 0x1) | ((status >> 1) & 0x1))
+    {
+        // Serial.println("Device is busy!!");
+
+        while(reqNum--) discard = i2cread();
+        return false;
+    }
+
+    rawData.x = (int16_t)((i2cread() << 8) | i2cread());
+    rawData.z = (int16_t)((i2cread() << 8) | i2cread());
+    rawData.y = (int16_t)((i2cread() << 8) | i2cread());
+
+    return true;
+}
+
+
+bool HMC5883L::getCalibData(void)
+{
+    if(!getRawData()) return false;
+    int16_t x = rawData.x, y = rawData.y, z = rawData.z;
+    calibData.x = (int16_t)(offset.x + x * (x >= 0 ? scale.xp : scale.xn));
+    calibData.y = (int16_t)(offset.y + y * (y >= 0 ? scale.yp : scale.yn));
+    calibData.z = (int16_t)(offset.z + z * (z >= 0 ? scale.zp : scale.zn));
+
+    return true;
+}
+
+
+bool HMC5883L::calibrate(void)
+{
+    int16_t offsetNum = 400;
+    int16_t scaleNum = 10;
+    resetData();
+    calibOffset(offsetNum);
+    calibScale(scaleNum);
+    return true;
+}
+
+
+bool HMC5883L::calibOffset(int16_t calibNum)
+{
+    bool _max = true, _min = false;
+    int8_t  bufferSize = 8, _idx = bufferSize;
+    int16_t x = rawData.x, y = rawData.y, _calibNum = (calibNum > 300 ? calibNum : 300);
+    int16_t _ofstXmin[bufferSize], _ofstYmin[bufferSize], _ofstXmax[bufferSize], _ofstYmax[bufferSize];
+    int16_t _sumXmin = 0, _sumXmax = 0, _sumYmin = 0, _sumYmax = 0;
+
+    bufferInitiate(_ofstXmin, x, bufferSize);
+    bufferInitiate(_ofstXmax, x, bufferSize);
+    bufferInitiate(_ofstYmin, y, bufferSize);
+    bufferInitiate(_ofstYmax, y, bufferSize);
+
+    Serial.println("********** Calibration(Offset) Start **********");
+    Serial.println("********** Keep the Compass Level and Rotate **********");
+
+    while(_calibNum--)
+    {
+        if(!getRawData()) {  _calibNum++; continue; }
+
+        getOffestBoundary(_ofstXmax, rawData.x, bufferSize, _max);
+        getOffestBoundary(_ofstXmin, rawData.x, bufferSize, _min);
+        getOffestBoundary(_ofstYmax, rawData.y, bufferSize, _max);
+        getOffestBoundary(_ofstYmin, rawData.y, bufferSize, _min);
+
+        Serial.print('.');
+        if(_calibNum % 100 == 0) Serial.println();
+        delay(30);
+    }
+
+    _sumXmax = sum(_ofstXmax, bufferSize);
+    _sumXmin = sum(_ofstXmin, bufferSize);
+    _sumYmax = sum(_ofstYmax, bufferSize);
+    _sumYmin = sum(_ofstYmin, bufferSize);
+
+    offset.x = -((_sumXmax >> 3) + (_sumXmin >> 3)) >> 1;
+    offset.y = -((_sumYmax >> 3) + (_sumYmin >> 3)) >> 1;
+    Serial.println("\n\rCheck result offset(x, y, z):\t");
+    printData(offset.x, offset.y, 0);
+
+    Serial.println("Calibration(Offset) Finished!");
+    return true; 
+}
+
+
+void HMC5883L::getOffestBoundary(int16_t *pBuffer, int16_t ref, int8_t bufferSize, bool sw)
+{
+    int8_t _idx = bufferSize;
+    
+    if(sw)
+    {
+        while(--_idx >= 0)
+        {  
+            if(ref > pBuffer[_idx]) 
+            { 
+                pBuffer[_idx] = ref;  
+                break; 
+            } 
+        } 
+    }
+    else
+    {
+        while(--_idx >= 0)
+        {  
+            if(ref < pBuffer[_idx]) 
+            { 
+                pBuffer[_idx] = ref;  
+                break; 
+            } 
+        }
+    }
+
+}
+
+
+bool HMC5883L::calibScale(int8_t calibNum)
+{
+    float _scaleXp = 1, _scaleYp = 1, _scaleZp = 1, _scaleXn = 1, _scaleYn = 1, _scaleZn = 1;
+
+    Serial.println("********** Calibration(Scale) Start **********");
+
+    setRegCfgA(HMC5883L_MEASMODE_P_BIAS, HMC5883L_DATARATE_75_HZ, HMC5883L_8_AVG_SAMPLE);
+    delay(300);
+
+    getBiasScale(&_scaleXp, &_scaleYp, &_scaleZp, calibNum);
+    Serial.println("Check positive scale(x, y, z):\t");
+    printData(_scaleXp, _scaleYp, _scaleZp);
+
+
+    setRegCfgA(HMC5883L_MEASMODE_N_BIAS, HMC5883L_DATARATE_75_HZ, HMC5883L_8_AVG_SAMPLE);
+    delay(300);
+
+    getBiasScale(&_scaleXn, &_scaleYn, &_scaleZn, calibNum);  
+    Serial.println("Check negative scale(x, y, z):\t");
+    printData(_scaleXn, _scaleYn, _scaleZn);
+
+    scale.xp = abs(_scaleXp);
+    scale.yp = abs(_scaleYp);
+    scale.zp = abs(_scaleZp);
+    scale.xn = abs(_scaleXn);
+    scale.yn = abs(_scaleYn);
+    scale.zn = abs(_scaleZn);
+
+    setRegCfgA(HMC5883L_MEASMODE_NORMAL, HMC5883L_DATARATE_75_HZ, HMC5883L_1_AVG_SAMPLE);
+    Serial.println("Calibration(Scale) Finished!");
+    return true;
+}
+
+
+void HMC5883L::getBiasScale(float *px, float *py, float *pz, int8_t calibNum)
+{
+    int8_t _calibNum = calibNum;
+
+    while(_calibNum--)
+    {
+        if(!getRawData()){
+            delay(20);
+            _calibNum++;
+            continue;
+        } 
+        *px += (float)rawData.x / (float)calibRef[(uint8_t)_deviceRange];
+        *py += (float)rawData.y / (float)calibRef[(uint8_t)_deviceRange];
+        *pz += (float)rawData.z / (float)calibRef[(uint8_t)_deviceRange];
+
+        delay(100);
+    }
+
+    *px /= (float)calibNum;
+    *py /= (float)calibNum;
+    *pz /= (float)calibNum;
+}
+
+
+void HMC5883L::resetData(void)
+{
+    int8_t count = 5;
+
+    scale.xp     = 1;
+    scale.yp     = 1;
+    scale.zp     = 1;
+    scale.xn     = 1;
+    scale.yn     = 1;
+    scale.zn     = 1;
+
+    offset.x     = 0;
+    offset.y     = 0;
+    offset.z     = 0;
+
+    rawData.x    = 0;
+    rawData.y    = 0;
+    rawData.z    = 0;
+    calibData.x  = 0;
+    calibData.y  = 0;
+    calibData.z  = 0;
+
+    while(count--)
+    {
+        while(!getRawData());
+        delay(30);
+    }
+}
+
+
+void HMC5883L::bufferInitiate(int16_t *pBuffer, int16_t value, int8_t bufferSize)
+{
+    int16_t _idx = bufferSize;
+    while(--_idx >= 0) pBuffer[_idx] = value;
+}
+
+
+int16_t HMC5883L::sum(int16_t *pBuffer, int8_t bufferSize)
+{
+    int16_t _idx = bufferSize;
+    int16_t _sum = 0;
+    while(--_idx >= 0) _sum += pBuffer[_idx];
+    return _sum;  
+}
+
+
+void HMC5883L::printData(int16_t x, int16_t y, int16_t z)
+{
+    Serial.print("\t");
+    Serial.print(x);
+    Serial.print(" ,\t");
+    Serial.print(y);
+    Serial.print(" ,\t");
+    Serial.println(z);
+}
+
+void HMC5883L::printData(float x, float y, float z)
+{
+    Serial.print("\t");
+    Serial.print(x);
+    Serial.print(" ,\t");
+    Serial.print(y);
+    Serial.print(" ,\t");
+    Serial.println(z);
+}
+
+
+void HMC5883L::printStatus(uint8_t status)
+{
+    Serial.print("HMC5883L Status: \n\r LOCK Bit: ");
+    Serial.print((status >> 1) & 0x1);
+    Serial.print(" ,\t READY bit: ");
+    Serial.println(status & 0x1);    
+}
+/*
 HMC5883L::HMC5883L()
 {
     m_Scale = 1;
@@ -210,3 +732,4 @@ char* HMC5883L::getErrorText(short errorCode)
 
     return "Error not defined.";
 }
+*/
--- HMC5883L.h
+++ HMC5883L.h
@@ -26,24 +26,150 @@
 #ifndef __HMC5883L_H__
 #define __HMC5883L_H__
 
-#include <Arduino.h>
-#include <Wire.h>
+
+/*=========================================================================
+    I2C ADDRESS/BITS
+    -----------------------------------------------------------------------*/
+#define HMC5883L_ADDRESS                    (0x1E)
+/*=========================================================================*/
+
+/*=========================================================================
+    REGISTERS
+    -----------------------------------------------------------------------*/
+#define HMC5883L_REG_CFG_A                  (0x00)  // Configuration Register A,    R/W
+#define HMC5883L_REG_CFG_B                  (0x01)  // Configuration Register B,    R/W
+#define HMC5883L_REG_MODE                   (0x02)  // Mode Register,               R/W
+#define HMC5883L_REG_X_MSB                  (0x03)  // Data Output X MSB Register,  R
+#define HMC5883L_REG_X_LSB                  (0x04)
+#define HMC5883L_REG_Y_MSB                  (0x05)
+#define HMC5883L_REG_Y_LSB                  (0x06)
+#define HMC5883L_REG_Z_MSB                  (0x07)
+#define HMC5883L_REG_Z_LSB                  (0x08)
+#define HMC5883L_REG_STATUS                 (0x09)
+#define HMC5883L_REG_ID_A                   (0x0A)
+#define HMC5883L_REG_ID_B                   (0x0B)
+#define HMC5883L_REG_ID_C                   (0x0C)
+/*=========================================================================*/
+
+#define ID_SUM                              (0x48 + 0x33 + 0x34)    // ID: 'H' + '3' + '4'
+
+#define REG_A_DEFAULT                       (0b01111111)            // bit[7] is reserved for future function. 
+                                                                    // set to 0 when configuring REG_A.
+
+#define REG_B_DEFAULT                       (0b11100000)            // bit[4] - bit[0] must be cleared for correct operation.
+
+#define REG_MODE_DEFAULT                    (0b00000011)            // bit[7] for high speed I2C, disabled.
+                                                                    // bit[6] - bit[2] reserved bits
+/*=========================================================================
+    Used with register 0x00 (HMC5883L_REG_CFG_A) to set these features
+    bit[7] reserved, set to 0.
+    -----------------------------------------------------------------------*/
+/* bit[6] - bit[5] */
+typedef enum
+{
+    HMC5883L_1_AVG_SAMPLE   =                  0b00,  // default
+    HMC5883L_2_AVG_SAMPLE   =                  0b01,
+    HMC5883L_4_AVG_SAMPLE   =                  0b10,
+    HMC5883L_8_AVG_SAMPLE   =                  0b11
+
+}   sampleAvg_t;
+
+/* bit[4] - bit[2] */
+typedef enum 
+{
+    HMC5883L_DATARATE_0_75_HZ   =              0b000,
+    HMC5883L_DATARATE_1_5_HZ    =              0b001,
+    HMC5883L_DATARATE_3_HZ      =              0b010,
+    HMC5883L_DATARATE_7_5_HZ    =              0b011,
+    HMC5883L_DATARATE_15_HZ     =              0b100,  // default
+    HMC5883L_DATARATE_30_HZ     =              0b101,
+    HMC5883L_DATARATE_75_HZ     =              0b110
+
+}   dataRate_t;
+
+/* bit[1] - bit[0] */
+typedef enum
+{
+    HMC5883L_MEASMODE_NORMAL    =              0b00,  // default
+    HMC5883L_MEASMODE_P_BIAS    =              0b01,
+    HMC5883L_MEASMODE_N_BIAS    =              0b10,
+
+}   measureMode_t;
+/*=========================================================================*/
+
+/*=========================================================================
+    Used with register 0x01 (HMC5883L_REG_CFG_B) to set sensor field range
+    bit[4] - bit[0] must be cleared for correct operation.
+    -----------------------------------------------------------------------*/
+/* bit[7] - bit[5] */
+typedef enum 
+{
+    HMC5883L_RANGE_0_88_GA       =              0b000,
+    HMC5883L_RANGE_1_3_GA        =              0b001,  // default
+    HMC5883L_RANGE_1_9_GA        =              0b010,
+    HMC5883L_RANGE_2_5_GA        =              0b011,
+    HMC5883L_RANGE_4_GA          =              0b100,
+    HMC5883L_RANGE_4_7_GA        =              0b101,
+    HMC5883L_RANGE_5_6_GA        =              0b110,
+    HMC5883L_RANGE_8_1_GA        =              0b111
+
+}   deviceRange_t;
+
+
+/*=========================================================================
+    Used with register 0x02 (HMC5883L_REG_MODE) to set operating mode
+    bit[7] Set this pin to enable High Speed I2C, 3400kHz.
+    -----------------------------------------------------------------------*/
+/* bit[1] - bit[0] */
+typedef enum 
+{
+    HMC5883L_OPMODE_CONTIUOUS   =               0b00,
+    HMC5883L_OPMODE_SINGLE      =               0b01,  // default
+    HMC5883L_OPMODE_IDLE        =               0b10,
+
+}   operateMode_t;
 
 
 
-#define HMC5883L_ADDRESS 0x1E
-#define CONFIGURATION_REGISTERA 0x00
-#define CONFIGURATION_REGISTERB 0x01
-#define MODE_REGISTER 0x02
-#define DATA_REGISTER_BEGIN 0x03
 
+struct rawData_t
+{
+    int16_t x;
+    int16_t y;
+    int16_t z;
+};
+
+struct calibData_t
+{
+    int16_t x;
+    int16_t y;
+    int16_t z;
+};
+
+struct scale_t
+{
+    float xp;
+    float yp;
+    float zp;
+    float xn;
+    float yn;
+    float zn;
+};
+
+struct offset_t
+{
+    int16_t x;
+    int16_t y;
+    int16_t z;
+};
+
+/*
 #define MEASUREMENT_CONTINUOUS 0x00
 #define MEASUREMENT_SINGLE_SHOT 0x01
 #define MEASUREMENT_IDLE 0x03
+*/
 
-#define ERRORCODE_1 "Entered scale was not valid, valid gauss values are: 0.88, 1.3, 1.9, 2.5, 4.0, 4.7, 5.6, 8.1"
-#define ERRORCODE_1_NUM 1
-
+/*
 struct MagnetometerScaled
 {
     float XAxis;
@@ -57,34 +183,58 @@ struct MagnetometerRaw
     short YAxis;
     short ZAxis;
 };
+*/
 
 class HMC5883L
-{
-
-public:         // used by xadow phone
-
-    void initCompass();
-    int getCompass();
-    
+{   
 public:
-    HMC5883L();
+    HMC5883L(void);
 
-    MagnetometerRaw readRawAxis();
-    MagnetometerScaled readScaledAxis();
-
-    short setMeasurementMode(uint8_t mode);
-    short setScale(float gauss);
-
-    char* getErrorText(short errorCode);
+    bool        begin(void);
+    uint8_t     getStatus(void);
+    void        writeRegister(uint8_t reg, uint8_t value);
+    uint8_t     readRegister(uint8_t reg);
     
-    
-protected:
+    void        setRange(deviceRange_t range);
+    void        setOperateMode(operateMode_t opMode);
+    void        setMeasureMode(measureMode_t measMode);
+    void        setSampleAvg(sampleAvg_t sample);
+    void        setDataRate(dataRate_t rate);
 
-    void write(short address, short byte);
-    uint8_t* read(short address, short length);
+    void        setRegCfgA(measureMode_t measMode, dataRate_t rate, sampleAvg_t sample);
+    void        setRegCfgB(deviceRange_t range);
+    void        setRegMode(operateMode_t opMode);
 
-    private:
-    float m_Scale;
+    bool        getRawData(void);
+    bool        getCalibData(void);
+    bool        calibrate(void);
+    bool        calibScale(int8_t calibNum);
+    bool        calibOffset(int16_t calibNum);
+
+    rawData_t       rawData;
+    calibData_t     calibData;
+    offset_t        offset;
+    scale_t         scale;
+
+private:
+    inline uint8_t      i2cread(void);
+    inline void         i2cwrite(uint8_t x);
+    bool                checkID(void);
+    void                resetData(void);
+    void                getBiasScale(float *px, float *py, float *pz, int8_t calibNum);
+    void                getOffestBoundary(int16_t *pBuffer, int16_t ref, int8_t bufferSize, bool sw);
+    void                printData(int16_t x, int16_t y, int16_t z);
+    void                printData(float x, float y, float z);
+    void                printStatus(uint8_t status);
+    int16_t             sum(int16_t *pBuffer, int8_t bufferSize);
+    void                bufferInitiate(int16_t *pBuffer, int16_t value, int8_t bufferSize);
+    operateMode_t       _operateMode;
+    deviceRange_t       _deviceRange;
+    measureMode_t       _measureMode;
+    dataRate_t          _dataRate;
+    sampleAvg_t         _sampleAvg;
+
+        // float m_Scale;
 };
 
 #endif
